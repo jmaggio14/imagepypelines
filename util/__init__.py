d80fe67310c64b00b67509df95a6fb4c0dc8d3a3
Jeff Maggio <jmaggio14@gmail.com>
2018-08-06 12:46:22 -0400

    updates init for timer

--- util/__init__.py
+++ util/__init__.py
@@ -1,3 +1,8 @@
 # output.py
 from .output import make_numbered_prefix
 from .output import prevent_overwrite
+
+# Timer.py
+from .timing import Timer
+from .timing import function_timer
+from .timing import function_timer_ms
