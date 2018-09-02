cafccb32494696e59db0de8859ca0c05c1334632
Jeff Maggio <jmaggio14@gmail.com>
2018-09-02 01:41:16 -0400

    adds special Exception for interpolation Checking

--- core/Exceptions.py
+++ core/Exceptions.py
@@ -1,4 +1,21 @@
-
+import imsciutils
 
 class CameraReadError(ValueError):
     pass
+
+class InvalidInterpolationType(ValueError):
+    def __init__(self,interp):
+        interp_string = """cv2.INTER_NEAREST --> {}
+                        cv2.INTER_LINEAR --> {}
+                        cv2.INTER_AREA --> {}
+                        cv2.INTER_CUBIC --> {}
+                        cv2.INTER_LANCZOS4 --> {}""".format(cv2.INTER_NEAREST,
+                                                            cv2.INTER_LINEAR,
+                                                            cv2.INTER_AREA,
+                                                            cv2.INTER_CUBIC,
+                                                            cv2.INTER_LANCZOS4,)
+        error_string = "'interpolation' ({}) must be one of the following!"\
+                                                            .format(interp)
+        error_string = error_string + '\n' + interp_string
+        imsciutils.error(error_string)
+        super(InvalidInterpolationType,self).__init__(error_string)
