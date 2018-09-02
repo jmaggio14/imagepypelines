2df1b5ae73bd8fc291883d130d4f97904d14373e
Jeff Maggio <jmaggio14@gmail.com>
2018-09-02 01:46:03 -0400

    renames logger to printout, and adds color printing

--- core/__init__.py
+++ core/__init__.py
@@ -2,12 +2,14 @@
 from .constants import *
 
 # logging.py
-from .logger import printmsg
-from .logger import debug
-from .logger import info
-from .logger import warning
-from .logger import error
-from .logger import critical
+from .printout import printmsg
+from .printout import debug
+from .printout import info
+from .printout import warning
+from .printout import error
+from .printout import critical
+from .printout import disable_printout_colors
+from .printout import enable_printout_colors
 
 # Exceptions
 from .Exceptions import CameraReadError
@@ -15,6 +17,7 @@ from .Exceptions import CameraReadError
 # standard_image.py
 from .standard_image import list_standard_images
 from .standard_image import get_standard_image
+from .standard_image import standard_image_input
 from .standard_image import lenna
 from .standard_image import lenna_gray
 from .standard_image import crowd
@@ -26,7 +29,6 @@ from .standard_image import gecko
 from .standard_image import roger
 from .standard_image import pig
 from .standard_image import carlenna
-from .standard_image import standard_image_input
 
 # ImageViewer.py
 from .ImageViewer import ImageViewer
