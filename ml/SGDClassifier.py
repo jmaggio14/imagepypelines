be272d40a4b0b1c85ddcf22e23d56de01e09150f
Jeff Maggio <jmaggio14@gmail.com>
2018-09-07 19:57:04 -0400

    placeholder for SGG/Software Classifier object

---
+++ ml/SGDClassifier.py
@@ -0,0 +1,14 @@
+
+
+
+class SGDClassifier(object):
+    def __init__(self,
+                    train_data,
+                    train_labels,
+                    test_data,
+                    test_labels,
+                    n_iter=10):
+        self.model = SGDClassifier(loss='log',n_iter)
+
+
+        model.fit()
