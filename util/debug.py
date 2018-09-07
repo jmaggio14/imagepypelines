7d78da1ea26842bc66799de0579c619103d6d31e
Jeff Maggio <jmaggio14@gmail.com>
2018-09-07 01:34:51 -0400

    reformats printouts into single string

--- util/debug.py
+++ util/debug.py
@@ -6,17 +6,30 @@ import imsciutils as iu
 def debug(exception):
 	"""
 	simple method to remove unecessary clutter in debugging
-	meant to be called exclusively in a try statement
-	simply prints the file,line and exeption has occured in more organized
+	meant to be called exclusively in a except statement
+	simply prints the file, line and exeption has occured in more organized
 	and easily readable fashion
 	"""
 	exc_type, exc_obj, tb = sys.exc_info()
-	fname = os.path.split(tb.tb_frame.f_code.co_filename)[1]
+	fname = os.path.basename(tb.tb_frame.f_code.co_filename)
 	line = tb.tb_lineno
 	traceback.print_tb(tb)
 	print("\r\n")
-	iu.error("===============================================================")
-	iu.error("                   | initital traceback | ")
-	iu.error("file: {0}\r\n\r\nline: {1} \r\n\r\ntype: {2}\r\n\r\n{3}\r\n"\
-                                        .format(fname,line,exc_type,exception))
-	iu.error("===============================================================")
+	error_msg = \
+	"""
+	===============================================================
+						| initital traceback |
+	file: {fname}
+
+	line: {line}
+
+	type: {exc_type}
+
+	{exception}
+	===============================================================
+	""".format(fname=fname,
+				line=line,
+				exc_type=exc_type,
+				exception=exception)
+
+	iu.error(error_msg)
