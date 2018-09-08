855feb3a19c68a593abcdc2a20ffdd9c04575e19
Jeff Maggio <jmaggio14@gmail.com>
2018-09-08 19:20:42 -0400

    adds imports for color.py

--- util/__init__.py
+++ util/__init__.py
@@ -1,10 +1,19 @@
-# timing.py
-from .timing import Timer
-from .timing import function_timer
-from .timing import function_timer_ms
-
+# color.py
+from .color import red
+from .color import blue
+from .color import green
+from .color import yellow
+from .color import color_text
 
 # error_checking.py
 from .error_checking import interpolation_type_check
 from .error_checking import dtype_type_check
 from .error_checking import is_numpy_array
+
+# permute.py
+from .Permuter import Permuter
+
+# timing.py
+from .timing import Timer
+from .timing import function_timer
+from .timing import function_timer_ms
