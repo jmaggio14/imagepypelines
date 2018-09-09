3cfd307398a0fa38abb62880f7b3a60ed494c313
Jeff Maggio <jmaggio14@gmail.com>
2018-09-09 00:21:36 -0400

    import datasets

--- ml/__init__.py
+++ ml/__init__.py
@@ -1,2 +1,8 @@
+# datasets.py
+from .datasets import get_mnist
+from .datasets import get_fashion_mnist
+from .datasets import get_cifar10
+from .datasets import get_cifar100
+
 #FeatureExtractor.py
 from .FeatureExtractor import FeatureExtractor
