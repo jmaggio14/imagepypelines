72374a1431e232d586bbe8c0300c9bc6d5e636e7
Jeff Maggio <jmaggio14@gmail.com>
2018-09-14 22:32:41 -0400

    init updates

--- util/__init__.py
+++ util/__init__.py
@@ -9,6 +9,8 @@ from .color import color_text
 from .error_checking import interpolation_type_check
 from .error_checking import dtype_type_check
 from .error_checking import is_numpy_array
+from .error_checking import is_iterable
+from .error_checking import type_error_message
 
 # permute.py
 from .Permuter import Permuter
