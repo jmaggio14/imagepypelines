60769d81d5ed37b5f450a37d9d95197bd0849280
Jeff Maggio <jmaggio14@gmail.com>
2018-09-22 14:45:24 -0400

    removes human tests from automated tests

--- test/automated_tests.py
+++ test/automated_tests.py
@@ -71,32 +71,6 @@ def test_normalize_and_bin():
     return True
 
 
-@iu.human_test
-def test_quick_image_view():
-    """THIS TEST REQUIRES A HUMAN TO VIEW THE IMAGE"""
-    testing_printer = iu.get_printer('TESTING')
-
-    pig = iu.pig()
-    iu.quick_image_view(pig,'quick_image_view test')
-
-
-@iu.human_test
-def test_number_image():
-    testing_printer = iu.get_printer('TESTING')
-
-    pig = iu.pig()
-    linear = iu.linear()
-
-    num_pig = iu.number_image(pig,1)
-    testing_printer.info("pig should be labeled '1'")
-    num_linear = iu.number_image(linear,2)
-    testing_printer.info("linear should be labeled '2'")
-
-    iu.quick_image_view(pig,title='pig')
-    testing_printer.info('PIG', iu.Summarizer(pig) )
-    iu.quick_image_view(linear,title='linear')
-    testing_printer.info('Linear', iu.Summarizer(linear) )
-
 
 
 
