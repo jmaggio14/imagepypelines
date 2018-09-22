bb2cf4b61bd712f6e4b55cb985de16a4863716c8
Jeff Maggio <jmaggio14@gmail.com>
2018-09-22 19:32:39 -0400

    updates version_info my email and the github URL

--- imsciutils/version_info.py
+++ imsciutils/version_info.py
@@ -4,3 +4,5 @@ __copyright__ = "Copyright 2018"
 __credits__ = ["Jeff Maggio", "Nathan Dileas", "Ryan Hartzell"]
 __license__ = "MIT"
 __status__ = "Experimental"
+__email__ = "jmaggio14@gmail.com"
+__url__ = 'https://github.com/jmaggio14/imsciutils'
