f713ac3c9c71187122a1bc923d702f944fa62aa3
Jeff Maggio <jmaggio14@gmail.com>
2018-09-25 19:17:29 -0400

    adds automatic header enforcement in all files

---
+++ enforcer.py
@@ -0,0 +1,169 @@
+#!/usr/bin/env python
+
+# MIT License
+#
+# Copyright (c) 2018 Jeff Maggio, Nathan Dileas, Ryan Hartzell
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+# @Author: Jeffrey Maggio, Nathan Dileas, Ryan Hartzell
+# @Date:   2018-09-25T18:42:16-04:00
+# @Email:  jmaggio14@gmail.com
+# @Project: imsciutils
+# @Filename: enforcer.py
+# @Last modified by:   Jeffrey Maggio, Nathan Dileas, Ryan Hartzell
+# @Last modified time: 2018-09-25T18:58:24-04:00
+# @License: MIT License
+import sys
+import os
+import glob
+
+SKIP = ['.git',
+        'LICENSE',
+        'Pipfile',
+        'Pipfile.lock',
+        'TODO.md',
+        'README.md',
+        'requirements.txt']
+
+ACCEPTABLE_EXTS = ['.py']
+
+LICENSE_HEADER = """
+@Email:  jmaggio14@gmail.com
+
+MIT License
+
+Copyright (c) 2018 Jeff Maggio, Nathan Dileas, Ryan Hartzell
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+EXTENSIONS_DICT = {'py': '#'}
+
+def generate_header(unformatted_license, filename):
+    """
+    Generates the list of strings specific to the given character as
+    a comment
+    :returns license header lines in list
+    """
+    extension = filename.split('/')[-1].split('\\')[-1].split('.')[-1]
+    if extension not in EXTENSIONS_DICT:
+        print('Warning: unknown extension associated with:', filename)
+        return []
+    char = EXTENSIONS_DICT[extension]
+    return [(char + ' ' + line).rstrip()\
+            for line in unformatted_license.split('\n')]
+
+def check_valid_header(header, filename):
+    """
+    Checks to see if the given file contents has a valid header on top
+    """
+    with open(filename, 'r') as filestream:
+        content = filestream.read()
+        if LICENSE_HEADER not in content:
+            return False
+    return True
+
+def print_valid_header(header, filename):
+    """
+    Simply checks to see if the header is valid, and prints accordingly
+    """
+    if check_valid_header(header, filename):
+        print('Valid:', filename)
+        return 0
+    print('Invalid', filename)
+    return -1
+
+def apply_header(header, filename):
+    """
+    Applies the generated license header to the target file, comment
+    for license header is generated through the extension dictionary
+    :return True on success, false if unknown extension
+    """
+    if print_valid_header(header, filename) == 0:
+        return 0
+    with open(filename, 'r+') as filestream:
+        content = filestream.read()
+        filestream.seek(0)
+        filestream.write("\n".join(header) + "\n")
+        filestream.write(content)
+    return 0
+
+def evaluate_header(header, filename, modify):
+    """
+    Runs either the apply_header or print_valid_header
+    depending on modify boolean
+    """
+    try:
+        if modify:
+            return apply_header(header, filename)
+        return print_valid_header(header, filename)
+    except UnicodeDecodeError:
+        return 0
+
+def enforce_header(unformatted_license, directory, modify=False):
+    """
+    Recursively iterates over target directory and finds all known
+    file types and checks to see whether or not they conform to the header
+    :arg modify If modify is set to True then it will inject the header if it
+    does not exist
+    :return 0 on success, -1 on failure
+    """
+    returns = 0
+    if os.path.isfile(os.path.abspath(directory)):
+        header = generate_header(unformatted_license, directory)
+        return evaluate_header(header, directory, modify)
+    filenames = []
+    for ext in ACCEPTABLE_EXTS:
+        filenames.extend( glob.glob(directory + '/**/*' + ext, recursive=True) )
+
+    for obj in filenames:
+        if obj not in SKIP:
+            if os.path.isfile(os.path.abspath(obj)):
+                header = generate_header(unformatted_license, obj)
+                if evaluate_header(header, obj, modify) == -1:
+                    returns = -1
+            elif enforce_header(unformatted_license,\
+                    os.path.join(directory, obj)) == -1:
+                returns = -1
+    return returns
+
+if __name__ == '__main__':
+    RESULT = enforce_header(LICENSE_HEADER, './imsciutils', True)
+    if RESULT == 0:
+        print('Successful enforcement')
+    else:
+        print('Failed enforcement')
+    sys.exit(RESULT)
