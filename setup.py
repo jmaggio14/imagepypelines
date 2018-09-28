5dc872f30dbc6cf4ee1f13a4acff14bf3e4231b4
Jeff Maggio <jmaggio14@gmail.com>
2018-09-28 09:31:57 -0400

    adds python 2.7 to setup 'classifiers'

--- setup.py
+++ setup.py
@@ -27,9 +27,7 @@ setup(name='imsciutils',
           'Topic :: Scientific/Engineering'
           'License :: OSI Approved :: MIT License',
           'Programming Language :: Python',
-          # 'Programming Language :: Python :: 2.7', # JM: currently the code is 3.5+ only
-          # 'Programming Language :: Python :: 3.3',
-          # 'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 2.7',
           'Programming Language :: Python :: 3.5',
           'Programming Language :: Python :: 3.6',
           'Programming Language :: Python :: 3.7',
