dcd2295dbe034b0f2995a485df5405f69fc1d166
Jeff Maggio <jmaggio14@gmail.com>
2018-10-03 08:21:04 -0400

    doctest compatible docstrings

--- imsciutils/ml/ConfigFactory.py
+++ imsciutils/ml/ConfigFactory.py
@@ -21,25 +21,25 @@ class ConfigFactory(object):
     from a sample of arguments and keyword arguments
 
     Example:
-        def run_important_test(arg1,arg2,arg3,first,second,third):
-            do_something_important()
+        >>> def run_important_test(arg1,arg2,arg3,first,second,third):
+        ...    do_something_important()
 
 
-        arg_trials = [
-                [1,2,3], # trials for first positional argument
-                ['a','b','c'], # trials for second positional arguments
-                ['y','z'], # trials for third positional argument
-                ]
+        >>> arg_trials = [
+        ...        [1,2,3], # trials for first positional argument
+        ...        ['a','b','c'], # trials for second positional arguments
+        ...        ['y','z'], # trials for third positional argument
+        ...        ]
 
-        kwarg_trials = {
-                    'first':None, # trials for 'first' keyword argument
-                    'second':['I','J','K'], # trials for 'second' keyword argument
-                    'third':['i','j','k'], # trials for 'third' keyword argument
-                    }
+        >>> kwarg_trials = {
+        ...            'first':None, # trials for 'first' keyword argument
+        ...            'second':['I','J','K'], # trials for 'second' keyword argument
+        ...            'third':['i','j','k'], # trials for 'third' keyword argument
+        ...            }
 
-        permuter = ConfigFactory(*arg_trials,**kwarg_trials)
-        for args,kwargs in permuter:
-            run_important_test(*args,**kwargs)
+        >>> permuter = ConfigFactory(*arg_trials,**kwarg_trials)
+        >>> for args,kwargs in permuter:
+        ...    run_important_test(*args,**kwargs)
 
     """
     def __init__(self,*arg_trials,**kwarg_trials):
