7df76bc400c5c656b6721fd87eca5a10f119b294
Jeff Maggio <jmaggio14@gmail.com>
2018-10-03 13:59:17 -0400

    add license's in all files

--- imsciutils/ml/builtin_blocks/__init__.py
+++ imsciutils/ml/builtin_blocks/__init__.py
@@ -1,3 +1,10 @@
+#
+# @Email:  jmaggio14@gmail.com
+#
+# MIT License: https://github.com/jmaggio14/imsciutils/blob/master/LICENSE
+#
+# Copyright (c) 2018 Jeff Maggio, Nathan Dileas, Ryan Hartzell
+#
 # add the name of any imports to this variable
 __all__ = ['Orb']
 
