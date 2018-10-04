c1ac03fc9d1ec151609d23f073f9c242b522a3ec
Jeff Maggio <jmaggio14@gmail.com>
2018-10-03 23:18:43 -0400

    (non-operable)WIP new pipelining system

--- imsciutils/core/BaseBlock.py
+++ imsciutils/core/BaseBlock.py
@@ -6,123 +6,216 @@
 # Copyright (c) 2018 Jeff Maggio, Nathan Dileas, Ryan Hartzell
 #
 from .Printer import get_printer
+def create_lazy_block(processing_fn,
+                        input_shape=None,
+                        output_shape=None,
+                        process_group=False,
+                        name=None,
+                        args=tuple(),
+                        kwargs={}):
 
+    # making the process_fn static so it can operate inside of a class
+    process_fn = staticmethod(processing_fn)
+    # create a new block using a metaclass instance
+    block_cls = type(processing_fn.__name__,
+                        (BaseBlock,),
+                        {'process_fn':process_fn})
+
+    block = block_cls(input_shape=input_shape,
+                        output_shape=output_shape,
+                        name=name,
+                        process_group=process_group,
+                        requires_training=False, #JM: func is only for lazy blocks
+                        args=args,
+                        kwargs=kwargs)
+    return block
+
+# class Transfer(object):
+#     def __init__(self,shape,):
+#         self.shape = shape
+#
+#     def compatible(self,shape):
+#         return self.shape == shape
 
 class BaseBlock(object):
-    """Base processing block object for constructing image processing pipelines
-
-    This object is meant to be subclassed for use in creating blocks for
-    use in `imsciutils` Pipeline objects.
-
-    Args:
-        name (str): the name of this block, default will be the name of the class
-
-    """
-    extant = {}
-    def __init__(self, name=None):
-        # JM: making printer for this class
+    self.EXTANT = {}
+    def __init__(self,
+                    input_shape,
+                    output_shape,
+                    name,
+                    process_group=False,
+                    requires_training=False,
+                    args=tuple(),
+                    kwargs={},):
+        # ----------- building a unique name for this block ------------
         if name is None:
             name = self.__class__.__name__
 
-        # keeping track of names internally in a class variable
-        if name in self.extant:
-            self.extant[name] += 1
+        # keeping track of names in base class variable
+        if name in self.EXTANT:
+            self.EXTANT[name] += 1
         else:
-            self.extant[name] = 1
-        name = name + str(self.extant[name])
+            self.EXTANT[name] = 1
+        name = name + str(self.EXTANT[name])
 
+        self.input_shape = input_shape
+        self.output_shape = output_shape
+        self.process_group = process_group
         self.name = name
-        self.printer = get_printer(name)
-        self.is_trained = False
+        self.requires_training = requires_training
+        self.args = args
+        self.kwargs = kwargs
 
-    def setup(self, **kwargs): #setups the special arguments for this block
-        """(Optional overload)checks the instantiation kwargs
+        self.trained = False
+        if not self.requires_training:
+            self.trained = True
 
-        Function to overload to check the instantiation keywords for this
-        processing block. This function is meant to replace the __init__
-        function
+        self.printer = get_printer(self.name)
 
-        Args:
-            kwargs (dict): dictionary of kwargs passed into __init__ to set up
+    def process_fn(self,data,*args,**kwargs):
+        raise NotImplementedError("'process_fn' must be overloaded")
 
-        Returns:
-            self: this function must return self
-
-        Example:
-            def setup(param1,param2='example'):
-                self.param1 = param1
-                self.param2 = param2
-        """
-        raise NotImplementedError("'setup' must be overloaded in all Block children") #setups the special arguments for this block
-
-    def train(self, x_data): #trains the feature generator if required
-        """(Optional overload)trains this processing block
-
-        If this processing block requires training, this function can be
-        overloaded to train the underlying algorithm
-
-        Args:
-            x_data (variable type): training data for this processing block
-
-        Returns:
-            None
-        """
+    def train(self,x_data):
         pass
 
-    def process(self, x_data): # applies the algorithm to the data
-        """(Required overload)applys this blocks algorithm to the input data
-
-        Applies this blocks algorithm to the input data and returns the
-        processed data
-
-        Args:
-            x_data (type specified in validate_data): the input data to apply
-                the algorithm too
-
-        Returns:
-            processed_data
-        """
-        raise NotImplementedError("'process' must be overloaded in all Block children")
-
-    def validate_data(x_data):#validates input data
-        """(Optional overload)validates the data is of the correct type
-
-        This function should be overloaded to check and make sure the input data
-        is of the correct type and shape such that this block won't throw a
-        nebulous error. This function is expected to throw an error if the
-        input data is invalid.
-        The primary purpose of this function is for traceability purposes so
-        that errors in pipeline construction can be easily tracked to their
-        genesis
-
-        Args:
-            x_data (input datatype): the input data to validate
-
-        Raises:
-            user defined error
-        """
-        pass
-
-    def run_train(self, x_data):#JM: only called by Pipeline
-        """convienence function to validate input data and train the block"""
-        self.validate_data(x_data)
+    def run_train(self,x_data):
         self.train(x_data)
-        self.is_trained = True
+        self.trained = True
 
-    def run_process(self, x_data): #JM: only called by Pipeline
-        """convienence function to validate input data and process the data"""
-        if not self.is_trained:
-            error_msg = "the block must be trained before processing"
-            self.printer.error(error_msg)
-            raise RuntimeError(error_msg)
+    def run_process(self,x_data):
+        if not self.trained:
+            error_msg = "'{}' must be trained before processing can occur"\
+                        .format(self.name)
+            raise RunTimeError(error_msg)
 
-        self.validate_data(x_data)
-        return self.process(x_data)
-
-
-
-
+        if process_group:
+            out = self.process_fn(x_data,*self.args,**self.kwargs)
+        else:
+            out = [self.process_fn(d,*self.args,**self.kwargs) for d in x_data]
+        return out
 
 
 
-# end
+
+#
+#
+#
+#
+# class BaseBlock(object):
+#     """Base processing block object for constructing image processing pipelines
+#
+#     This object is meant to be subclassed for use in creating blocks for
+#     use in `imsciutils` Pipeline objects.
+#
+#     Args:
+#         name (str): the name of this block, default will be the name of the class
+#
+#     """
+#     extant = {}
+#     def __init__(self, name=None):
+#         # JM: making printer for this class
+#         if name is None:
+#             name = self.__class__.__name__
+#
+#         # keeping track of names internally in a class variable
+#         if name in self.extant:
+#             self.extant[name] += 1
+#         else:
+#             self.extant[name] = 1
+#         name = name + str(self.extant[name])
+#
+#         self.name = name
+#         self.printer = get_printer(name)
+#         self.is_trained = False
+#
+#     def setup(self, **kwargs): #setups the special arguments for this block
+#         """(Optional overload)checks the instantiation kwargs
+#
+#         Function to overload to check the instantiation keywords for this
+#         processing block. This function is meant to replace the __init__
+#         function
+#
+#         Args:
+#             kwargs (dict): dictionary of kwargs passed into __init__ to set up
+#
+#         Returns:
+#             self: this function must return self
+#
+#         Example:
+#             def setup(param1,param2='example'):
+#                 self.param1 = param1
+#                 self.param2 = param2
+#         """
+#         raise NotImplementedError("'setup' must be overloaded in all Block children") #setups the special arguments for this block
+#
+#     def train(self, x_data): #trains the feature generator if required
+#         """(Optional overload)trains this processing block
+#
+#         If this processing block requires training, this function can be
+#         overloaded to train the underlying algorithm
+#
+#         Args:
+#             x_data (variable type): training data for this processing block
+#
+#         Returns:
+#             None
+#         """
+#         pass
+#
+#     def process(self, x_data): # applies the algorithm to the data
+#         """(Required overload)applys this blocks algorithm to the input data
+#
+#         Applies this blocks algorithm to the input data and returns the
+#         processed data
+#
+#         Args:
+#             x_data (type specified in validate_data): the input data to apply
+#                 the algorithm too
+#
+#         Returns:
+#             processed_data
+#         """
+#         raise NotImplementedError("'process' must be overloaded in all Block children")
+#
+#     def validate_data(x_data):#validates input data
+#         """(Optional overload)validates the data is of the correct type
+#
+#         This function should be overloaded to check and make sure the input data
+#         is of the correct type and shape such that this block won't throw a
+#         nebulous error. This function is expected to throw an error if the
+#         input data is invalid.
+#         The primary purpose of this function is for traceability purposes so
+#         that errors in pipeline construction can be easily tracked to their
+#         genesis
+#
+#         Args:
+#             x_data (input datatype): the input data to validate
+#
+#         Raises:
+#             user defined error
+#         """
+#         pass
+#
+#     def run_train(self, x_data):#JM: only called by Pipeline
+#         """convienence function to validate input data and train the block"""
+#         self.validate_data(x_data)
+#         self.train(x_data)
+#         self.is_trained = True
+#
+#     def run_process(self, x_data): #JM: only called by Pipeline
+#         """convienence function to validate input data and process the data"""
+#         if not self.is_trained:
+#             error_msg = "the block must be trained before processing"
+#             self.printer.error(error_msg)
+#             raise RuntimeError(error_msg)
+#
+#         self.validate_data(x_data)
+#         return self.process(x_data)
+#
+#
+#
+#
+#
+#
+#
+# # end
--- imsciutils/core/Pipeline.py
+++ imsciutils/core/Pipeline.py
@@ -8,23 +8,25 @@
 from .Printer import get_printer
 from .BaseBlock import BaseBlock
 from .. import util
+import pickle
+import collections
 
 
 class Pipeline(object):
     """Pipeline object to apply a sequence of algorithms to input data
 
     """
-    extant = {}
-    def __init__(self, blocks=[], name=None, verbose=True):
+    EXTANT = {}
+    def __init__(self, name=None, blocks=[], verbose=True):
         if name is None:
             name = self.__class__.__name__
 
         # keeping track of names internally in a class variable
-        if name in self.extant:
-            self.extant[name] += 1
+        if name in self.EXTANT:
+            self.EXTANT[name] += 1
         else:
-            self.extant[name] = 1
-        name = name + str(self.extant[name])
+            self.EXTANT[name] = 1
+        name = name + str(self.EXTANT[name])
 
         self.name = name
         self.verbose = verbose
@@ -42,6 +44,7 @@ class Pipeline(object):
 
         self.blocks = blocks
 
+
     def add(self, block):
         """adds processing block to the pipeline processing chain"""
         # checking to make sure block is a real block
@@ -53,32 +56,101 @@ class Pipeline(object):
         # appends to instance block list
         self.blocks.append(block)
 
-    def train(self, x_data):
-        """trains all processing blocks in the pipeline"""
-        timer = util.Timer()
-        # runs training for each block and then processes the data for
-        # the next block to train with
-        for block in self.blocks:
-            block.run_train(x_data)
-            x_data = block.run_process(x_data)
-            # printing for traceability
-            self.printer.info("{}: trained in {} seconds on {} datums".format(
-                                                                block.name,
-                                                                timer.lap(),
-                                                                len(x_data)))
-        self.printer.info("all blocks trained: {}seconds".format(timer.time()))
-        return x_data # trains all blocks if required
+    def validate(self):
+        #JM: get output_shapes, last block's output is irrelevant
+        out_blocks = [b for b in self.blocks[:-1]]
+        #JM: get input blocks, first block's input is irrelevant
+        in_blocks = [b for b in self.blocks[1:]]
 
-    def process(self, x_data):
-        """processed x_data by sequentially running data through all blocks"""
-        timer = util.Timer()
-        # process the data through each block
-        for block in self.blocks:
-            x_data = block.run_process(x_data)
-            # printing for traceability
-            self.printer.info("{}: trained in {} seconds on {} datums".format(
-                                                                block.name,
-                                                                timer.lap(),
-                                                                len(x_data)))
+        raise_error = False
+        for b_out,b_in in zip(out_blocks,in_blocks):
+            if not (b_out.output_shape == b_in.input_shape):
+                error_msg = "incompatible shapes between {}-->{}".format(
+                                                                b_out.name,
+                                                                b_in.name)
+                self.printer.error(error_msg)
+                raise_error = True
+
+        # JM: TODO: make special exception for this purpose
+        if raise_error:
+            crit_msg = "All blocks must have compatible shapes"
+            self.printer.critical(crit_msg)
+            raise RuntimeError(crit_msg)
+
+    def train(self,x_data):
+        self.validate()
+        
+
+    def process(self,x_data):
+        self.validate()
+        # JM: TODO: add auto batching and intermediate data retrieval
+        # JM: verifying that all blocks have been trained
+        if not all(b.trained for b in self.blocks):
+            for b in self.blocks:
+                if b.trained:
+                    continue
+                err_msg = "requires training, but hasn't yet been trained"
+                self.printer.error("{}: ".format(b.name), err_msg)
+            raise RuntimeError("you must run Pipeline.train before processing")
+
+        # JM: processing all data
+        t = util.Timer()
+        for b in self.blocks:
+            num = len(x_data)
+            x_data = b.run_process(x_data)
+            b_time = t.lap() # time for this block
+            # printing time for this block
+            self.printer.info("{}: processed in {} seconds".format(b.name,
+                                                                    b_time))
+            # printing individual datum time
+            self.printer.debug("(approx {}sec per datum)".format(
+                                                        round(b_time / num,3)))
+
+
+
+        self.printer.info("all data processed in {} seconds".format(t.time()))
 
         return x_data
+
+
+
+    def graph(self):
+        """TODO: Placeholder function for @Ryan to create"""
+        pass
+
+    def save(self,filename=None):
+        if filename is None:
+            filename = self.name + '.pck'
+        with open(filename,'wb') as f:
+            pickle.dump(self,f)
+
+    #
+    # def train(self, x_data):
+    #     """trains all processing blocks in the pipeline"""
+    #     timer = util.Timer()
+    #     # runs training for each block and then processes the data for
+    #     # the next block to train with
+    #     for block in self.blocks:
+    #         block.run_train(x_data)
+    #         x_data = block.run_process(x_data)
+    #         # printing for traceability
+    #         self.printer.info("{}: trained in {} seconds on {} datums".format(
+    #                                                             block.name,
+    #                                                             timer.lap(),
+    #                                                             len(x_data)))
+    #     self.printer.info("all blocks trained: {}seconds".format(timer.time()))
+    #     return x_data # trains all blocks if required
+    #
+    # def process(self, x_data):
+    #     """processed x_data by sequentially running data through all blocks"""
+    #     timer = util.Timer()
+    #     # process the data through each block
+    #     for block in self.blocks:
+    #         x_data = block.run_process(x_data)
+    #         # printing for traceability
+    #         self.printer.info("{}: trained in {} seconds on {} datums".format(
+    #                                                             block.name,
+    #                                                             timer.lap(),
+    #                                                             len(x_data)))
+    #
+    #     return x_data
