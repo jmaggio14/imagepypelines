b399cfaa2cbe3a200541392d5f6a3d248796df19
Jeff Maggio <jmaggio14@gmail.com>
2018-10-07 16:15:22 -0400

    adds test Resizer Block

---
+++ imsciutils/core/builtin_blocks/Resizer.py
@@ -0,0 +1,25 @@
+from .. import SimpleBlock
+import cv2
+
+class Resizer(SimpleBlock):
+    def __init__(self,
+                    to_height=512,
+                    to_width=512,
+                    interpolation=cv2.INTER_NEAREST,
+                    name=True):
+        self.to_height = to_height
+        self.to_width = to_width
+        self.interpolation
+
+        input_shape = [None,None], [None,None,3]
+        output_shape = [None,None], [None,None,3]
+
+        super(Resizer,self).__init__(input_shape=input_shape,
+                                            output_shape=output_shape,
+                                            name=name,
+                                            requires_training=False)
+    def process(self,datum):
+        resize = cv2.resize(datum,
+                       dsize=(self.to_width,self.to_height),
+                       interpolation=self.interpolation)
+        return resized
