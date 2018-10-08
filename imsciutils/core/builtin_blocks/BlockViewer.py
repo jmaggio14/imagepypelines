6734f8ebf4da649caf902a80219b292a11a03425
Jeff Maggio <jmaggio14@gmail.com>
2018-10-07 20:31:38 -0400

    Adds testing block viewer

---
+++ imsciutils/core/builtin_blocks/BlockViewer.py
@@ -0,0 +1,18 @@
+from .. import SimpleBlock
+from ..Viewer import Viewer
+import time
+
+class BlockViewer(SimpleBlock):
+    def __init__(self,pause_time=0.1):
+        self.pause_time = pause_time
+        input_shape = None
+        output_shape = None
+        super(BlockViewer,self).__init__(input_shape,
+                                        output_shape,
+                                        requires_training=False)
+        self.viewer = Viewer(self.name)
+
+    def process(self,datum):
+        self.viewer.view(datum)
+        time.sleep(self.pause_time)
+        return datum
