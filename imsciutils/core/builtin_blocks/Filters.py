ff4bb4f79fa331a6c1f7a699cbade0d655247bfc
Jeff Maggio <jmaggio14@gmail.com>
2018-10-12 16:13:12 -0400

    adds header and license

--- imsciutils/core/builtin_blocks/Filters.py
+++ imsciutils/core/builtin_blocks/Filters.py
@@ -1,3 +1,10 @@
+#
+# @Email:  jmaggio14@gmail.com
+#
+# MIT License: https://github.com/jmaggio14/imsciutils/blob/master/LICENSE
+#
+# Copyright (c) 2018 Jeff Maggio, Nathan Dileas, Ryan Hartzell
+#
 from .. import SimpleBlock
 from .. import ArrayType
 from ..coordinates import dimensions
