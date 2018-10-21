f61820898e569f5d11d436818a1245c9d24d367f
Jeff Maggio <jmaggio14@gmail.com>
2018-10-21 12:11:28 -0400

    adds license header

--- imsciutils/core/builtin_blocks/PCA.py
+++ imsciutils/core/builtin_blocks/PCA.py
@@ -1,3 +1,10 @@
+#
+# @Email:  jmaggio14@gmail.com
+#
+# MIT License: https://github.com/jmaggio14/imsciutils/blob/master/LICENSE
+#
+# Copyright (c) 2018 Jeff Maggio, Nathan Dileas, Ryan Hartzell
+#
 
 from sklearn.decomposition import PCA as sklearn_PCA
 from .. import BatchBlock
