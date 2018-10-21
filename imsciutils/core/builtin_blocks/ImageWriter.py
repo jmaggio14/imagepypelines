2eb8fffd1f54c491e0ef3d36869a322fe97f9e5b
Jeff Maggio <jmaggio14@gmail.com>
2018-10-21 18:37:02 -0400

    adds separate directories for every image label

--- imsciutils/core/builtin_blocks/ImageWriter.py
+++ imsciutils/core/builtin_blocks/ImageWriter.py
@@ -35,9 +35,7 @@ class ImageWriter(SimpleBlock):
             filename common among all images, these will be incremented
             numerically with each new image saved
     """
-    def __init__(self,output_dir,
-                        base_filename='image.png',
-                        ):
+    def __init__(self,output_dir, base_filename='image.png'):
 
         assert isinstance(output_dir, str), "'output_dir' must be str"
         assert isinstance(base_filename, str), "'base_filename' must be str"
@@ -47,7 +45,8 @@ class ImageWriter(SimpleBlock):
         self.output_dir = output_dir
         self.base_filename = base_filename
         self.image_number = 0
-
+        self.batch_dirs = []
+        self.batch_index = 0
 
         io_map = {ArrayType([None,None,3]):ArrayType([None,None,3]),
                     ArrayType([None,None]):ArrayType([None,None])}
@@ -56,11 +55,35 @@ class ImageWriter(SimpleBlock):
                                             requires_training=False,
                                             requires_labels=False)
 
+    def before_process(self,data,labels):
+        """generates output directories for each image in the batch
+
+        Args:
+            data(list): list of images to save
+            labels(list): labels for each image
+
+        Returns:
+            None
+        """
+        # JM: if integer labels are given, then create different output
+        # directories for each new label
+        if all(isinstance(lbl,int) for lbl in labels):
+            self.batch_dirs = \
+                [os.path.join(self.output_dir,str(lbl)) for lbl in labels]
+        # JM: otherwise create the same output directory for each image
+        else:
+            self.batch_dirs = [self.output_dir] * len(data)
+
+        # create output directories if they don't already exist
+        uniques = set(self.batch_dirs)
+        for out_dir in uniques:
+            if not os.path.exists(out_dir):
+                os.makedirs(out_dir)
+
+        self.batch_index = 0
 
     def process(self, datum):
-        """
-        writes an image frame to the specificed directory, forces
-        resizing if specified when the class is instantiated
+        """writes an image frame to the specificed directory
 
         Args:
             datum (np.ndarray): frame to be saved to the output_dir
@@ -70,13 +93,12 @@ class ImageWriter(SimpleBlock):
         """
         self.image_number += 1
         image_number = util.make_numbered_prefix(self.image_number,6)
-        out_filename = os.path.join(self.output_dir,
-                                    image_number + self.base_filename)
-
-        if not isinstance(self.size, type(None)):
-            datum = cv2.resize(datum,
-                               dsize=(self.size[1], self.size[0]),
-                               interpolation=self.interpolation)
+        filename = os.path.join(self.batch_dirs[self.batch_index],
+                                        image_number + self.base_filename)
 
         cv2.imwrite(filename, datum)
+        self.batch_index += 1
         return datum
+
+    def after_process(self):
+        self.batch_dirs = []
