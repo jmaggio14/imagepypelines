b373cfd70c2922cccbfaef30a1b180b04de87fba
Jeff Maggio <jmaggio14@gmail.com>
2018-10-25 15:23:14 -0400

    deletes old pre-pipeline class

--- imsciutils/ml/__init__.py
+++ imsciutils/ml/__init__.py
@@ -11,9 +11,6 @@ from .datasets import get_cifar10
 from .datasets import get_cifar100
 from .datasets import dataset_to_list
 
-# FeatureExtractor.py
-from .FeatureExtractor import FeatureExtractor
-
 # FisherVectors.py
 from .FisherVectors import FisherVectors
 
