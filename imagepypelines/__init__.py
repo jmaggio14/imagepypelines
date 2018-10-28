d0b9cef4baff9cacd203202fae9ac09b38c8d71b
Jeff Maggio <jmaggio14@gmail.com>
2018-10-27 22:59:09 -0400

    adds auto-checking for packages that can't be included in the setup.py

--- imagepypelines/__init__.py
+++ imagepypelines/__init__.py
@@ -4,6 +4,36 @@
 # @github: https://github.com/jmaggio14/imagepypelines
 #
 # Copyright (c) 2018 Jeff Maggio, Nathan Dileas, Ryan Hartzell
+
+# check if the dependencies that aren't included in the setup.py are installed
+import subprocess
+import sys
+import os
+
+# get list of installed packages, silence output
+FNULL = open(os.devnull,'w')
+reqs = subprocess.check_output([sys.executable, '-m', 'pip', 'freeze'],stderr=FNULL)
+installed_packages = [r.decode().split('==')[0] for r in reqs.split()]
+FNULL.close()
+
+# import pdb; pdb.set_trace()
+
+# check for tensorflow
+if 'tensorflow' not in installed_packages:
+    print("ERROR: tensorflow must be installed for imagepypelines to operate!")
+    print("'pip install tensorflow --user' for CPU only")
+    print("pip install tensorflow-gpu --user' for CPU+GPU")
+    print("see README for details: https://github.com/jmaggio14/imagepypelines")
+    sys.exit(1)
+# check for opencv
+if 'cv2' not in installed_packages:
+    print("ERROR: opencv must be installed for imagepypelines to operate!")
+    print("see README for details: https://github.com/jmaggio14/imagepypelines")
+    sys.exit(1)
+
+
+
+
 import pkg_resources
 STANDARD_IMAGE_DIRECTORY = pkg_resources.resource_filename(__name__,
                                                         'data/standard_images')
@@ -14,3 +44,6 @@ from .core import *
 from . import util
 from . import ml
 from . import io
+
+# delete namespace pollutants
+del subprocess, sys, pkg_resources, os
