f3efbc796851996849842ff05ebe46808b58b1a0
Jeff Maggio <jmaggio14@gmail.com>
2018-11-24 00:23:59 -0500

    rewrites Cache class - still in WIP phase

--- imagepypelines/core/caching.py
+++ imagepypelines/core/caching.py
@@ -1,90 +1,94 @@
+# @Email: jmaggio14@gmail.com
+# @Website: https://www.imagepypelines.org/
+# @License: https://github.com/jmaggio14/imagepypelines/blob/master/LICENSE
+# @github: https://github.com/jmaggio14/imagepypelines
+#
+# Copyright (c) 2018 Jeff Maggio, Nathan Dileas, Ryan Hartzell
+#
 import os
 from uuid import uuid4
 import glob
-from .. import CACHE, CACHE_TMP, CACHE_META, CACHE_DATASETS
-from .Printer import get_printer
 import pickle
 
-class Cache(dict):
+from .. import CACHE, CACHE_TMP, CACHE_META, CACHE_DATASETS
+from .Printer import get_printer
+from .Pipeline import Pipeline, restore_from_file
+from .BaseBlock import BaseBlock
+
+
+
+class Cache(object):
     subdir = CACHE
-    printer = get_printer(CACHE)
+    def filename(self,basename="no-key"):
+        base,ext = os.path.split(basename)
+        basename = "{}.{}.{}".format(base,
+                                        uuid4().hex[:8],
+                                        ext)
 
-    def __init__(self,*args,**kwargs):
-        self.hashes = {}
-        self.filenames = {}
-        super(Cache,self).__init__(*args,**kwargs)
-
-    def get_filename(self,key="no-key"):
-        return "{}.{}".format(key,uuid4().hex[:8])
-
-    def __setitem__(self,key,data):
-        assert isinstance(key,str), "Cache keys must be a string"
-        if key in self:
-            self.printer.warning(
-                        "replacing {}".format(self.filenames[key]) )
-            self.delete( self.filenames[key] )
-
-        self.filenames[key] = self.get_filename(key)
-        with open(self[key],'wb') as f:
-            pickled_obj = pickle.dumps(data)
-            self.hashes[key] = hash(pickled_obj)
-            f.write(pickled_obj)
-
-    def __getitem__(self,key):
-        filename = self.filenames[key]
-        self.printer.info("retrieving {}".format(filename))
-        with open(filename,'rb') as f:
-            pickled_obj = f.read()
-            data = pickle.loads(pickled_obj)
-
-        return data
-
-    def __delitem__(self,key):
-        self.delete(key)
+        fname = os.path.join(self.subdir,basename)
+        path = os.path.split(fname)[0]
+        if not os.path.exists(path):
+            os.makedirs(path)
+        return fname
 
     def listdir(self):
         return glob.glob( os.path.join(self.subdir,'*') )
-
-    def remove(self,key_or_fname):
-        if key_or_fname in self:
-            self.printer.warning("deleting {}".format(key_or_fname))
-            del self[key_or_fname]
-            del self.filenames[key_or_fname]
-            del self.hashes[key_or_fname]
-            os.remove( self.filenames[key_or_fname] )
-        else:
-            self.printer.warning("deleting {}".format(key_or_fname))
-            os.remove(key_or_fname)
 
     def purge(self):
         for fname in self.listdir():
             self.remove(fname)
 
+    def remove(self,fname):
+        os.remove(fname)
+
+    def save(self,obj):
+        # if it's a block, we can run it's prep_for_serialization
+        # function to ensure serialization compatability
+        if isinstance(obj,BaseBlock):
+            obj.prep_for_serialization()
+            fname = self.filename( str(obj) + '.pck' )
+            self.printer.info("saving {} to {}".format(obj,fname)
+            with open(fname,'wb') as f:
+                pickle.dump(obj,f)
+
+        # if it's a pipeline, we can use the pipeline's own save
+        # function
+        elif isinstance(obj,Pipeline):
+            fname = self.filename( str(obj) + '.pck' )
+            obj.save(fname)
+
+        # otherwise if it's a generic object, we can save it
+        else:
+            fname = self.filename(obj.__class__.__name__ + '.pck')
+            self.printer.info("saving {} to {}".format(obj,fname)
+            with open(fname,'wb') as f:
+                pickle.dump(obj,f)
+
+    def retrieve(self,fname):
+        obj = pickle.loads(fname)
+
+        if isinstance(obj,'BaseBlock'):
+            obj.prep_for_serialization()
+            return obj
+
+        elif isinstance(obj,Pipeline):
+            return restore_from_file(fname)
+
+        return obj
 
 
-# -------- functions for ~/.imagepypelines/tmp ---------------
+
+
+
 class TMP(Cache):
     subdir = CACHE_TMP
 
-    def __del__(self):
-        self.purge()
-
-# -------- functions for ~/.imagepypelines/metadata ---------------
-class METADATA(Cache):
+class META(Cache):
     subdir = CACHE_META
 
-# -------- functions for ~/.imagepypelines/datasets ---------------
 class DATASETS(Cache):
     subdir = CACHE_DATASETS
 
-
-
 tmp = TMP()
-metadata = METADATA()
+metadata = META()
 datasets = DATASETS()
-
-
-def purge_cache():
-    tmp.purge()
-    metadata.purge()
-    datasets.purge()
