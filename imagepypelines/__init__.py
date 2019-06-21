41064903d37f93ca9908752284bac29678c86b05
Jeff Maggio <jmaggio14@gmail.com>
2019-06-20 21:35:31 -0400

    adds session uuid

--- imagepypelines/__init__.py
+++ imagepypelines/__init__.py
@@ -5,26 +5,32 @@
 #
 # Copyright (c) 2018-2019 Jeff Maggio, Nathan Dileas, Ryan Hartzell
 
-# enable terminal colors if we are in an nt system
+# -------- setup a uuid for this imagepypelines session -------
+import time
+from uuid import uuid4
+init_time = time.time()
+"""initiatization time for this imagepypelines session"""
+session_uuid = uuid4().hex
+"""a universally unique id for this imagepypelines session"""
+
+# --------- enable terminal colors if we are in on a windows system ---------
 import os
 if os.name == 'nt':
     import colorama
     colorama.init()
     del colorama
 
-
-
+# --------- retrieve the source directory for our standard images ---------
 import pkg_resources
-# retrieve the source directory for our standard images
 STANDARD_IMAGE_DIRECTORY = pkg_resources.resource_filename(__name__,
                                                         'data/standard_images')
-
+"""the location where imagepypelines standard images are stored"""
 # -------- setup a cache directory on the local machine -------
 CACHE = os.path.join(os.path.expanduser('~'),'.imagepypelines')
+"""the local imagepypelines cache/config directory for this user"""
 
 # ---------- delete namespace pollutants ----------
-del pkg_resources, os
-
+del pkg_resources, os, uuid4, time
 
 # ---------- import imagepypelines ----------
 from .version_info import *
