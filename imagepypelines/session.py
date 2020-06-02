7b2e41959cecac1426f356e4a44795c635a56d2b
RyanHartzell <rah3156@rit.edu>
2020-06-02 01:57:40 -0400

    Adds header, whoops

--- imagepypelines/session.py
+++ imagepypelines/session.py
@@ -1,1 +1,7 @@
+"""@Email: jmaggio14@gmail.com
+@Website: https://www.imagepypelines.org/
+@License: https://github.com/jmaggio14/imagepypelines/blob/master/LICENSE
+@github: https://github.com/jmaggio14/imagepypelines
+Copyright (c) 2018-2020 Jeff Maggio, Ryan Hartzell, and collaborators
+"""
 from listener import Manager, Listener
