bfbef8a5d9338c234aee3d2937c459703614a862
RyanHartzell <rah3156@rit.edu>
2020-06-02 02:04:24 -0400

    Trying this again with hopefully the right header

--- imagepypelines/session.py
+++ imagepypelines/session.py
@@ -1,7 +1,9 @@
-"""@Email: jmaggio14@gmail.com
-@Website: https://www.imagepypelines.org/
-@License: https://github.com/jmaggio14/imagepypelines/blob/master/LICENSE
-@github: https://github.com/jmaggio14/imagepypelines
-Copyright (c) 2018-2020 Jeff Maggio, Ryan Hartzell, and collaborators
-"""
+# @Email: jmaggio14@gmail.com
+# @Website: https://www.imagepypelines.org/
+# @License: https://github.com/jmaggio14/imagepypelines/blob/master/LICENSE
+# @github: https://github.com/jmaggio14/imagepypelines
+# 
+# Copyright (c) 2018-2020 Jeff Maggio, Ryan Hartzell, and collaborators
+#
+
 from listener import Manager, Listener
