fa2debd41cbbd32e81a41570cb9984662c620c76
Jeff <jmaggio14@gmail.com>
2020-07-12 11:47:03 -0400

    adds license to new files

--- imagepypelines/DashboardComm.py
+++ imagepypelines/DashboardComm.py
@@ -1,3 +1,10 @@
+# @Email: jmaggio14@gmail.com
+# @Website: https://www.imagepypelines.org/
+# @License: https://github.com/jmaggio14/imagepypelines/blob/master/LICENSE
+# @github: https://github.com/jmaggio14/imagepypelines
+#
+# Copyright (c) 2018 - 2020 Jeff Maggio, Jai Mehra, Ryan Hartzell
+#
 """
 # this is an example use case:
 
