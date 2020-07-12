857f4f893f07b22028c5149f90e891cec4727a39
Jai Mehra <jmjaiken@gmail.com>
2020-07-11 22:26:18 -0400

    Deprecated Listener, un-needed as of now but has simulations functions

--- imagepypelines/[OLD]Listener.py
+++ imagepypelines/[OLD]Listener.py
@@ -33,66 +33,66 @@ logging.basicConfig(level=logging.DEBUG)
 
 
 # __ Parent Thread Manager Class _____________________________________________
-class BaseCommThread(threading.Thread):
-    '''
-    Parent Class to all thread manager classes.
-    '''
-    def __init__(self):
-        super().__init__(name=self.__class__.__name__)
-        self.daemon = True
-
-    def __enter__(self):
-        '''
-        Starts the thread in its own context manager block.
-        Note: If the running thread is meant to be run indefinitely it is not
-              recommended to use it as a context manager as once you exit the
-              context manager, the thread will safely shut down via the
-              __exit__() method.
-        '''
-        self.run()
-
-    def __exit__(self, exc_type, exc_value, traceback):
-        '''
-        Is called once the context leaves the manager, safely signals the
-        running thread to shutdown.
-        '''
-        self.stop_thread()
-
-    # ____ Run Function ______________________________________________________
-    def run(self):
-        '''
-        This function is to be overloaded in the child class. If the thread is
-        to be run indefinitely (as in not for a fixed duration), you MUST
-        structure this function as follows:
-
-        --[START]--------------------------------------------------------------
-        self.t = threading.current_thread()  # Grab current threading context
-        ...
-        while getattr(self.t, 'running', True):
-            ...
-        ...
-        --[END]--------------------------------------------------------------
-
-        This is necessary as the classes stop_thread() method can safely shut
-        down the running thread by changing self.running to False, thus
-        invalidating the while loop's condition.
-        '''
-        pass
-
-    # ____ Thread Killer _____________________________(Kills with kindness)___
-    def stop_thread(self):
-        '''
-        This is a convenience function used to safely end the running thread.
-        Note: This will only end the running thread if the run() function
-              checks for the classes 'running' attribute (as demonstrated in
-              the docstring of the run() function above).
-              This only works if the running thread is not hanging, this will
-              prevent the while loop from re-evaluating its condition
-        '''
-        logging.warning("Closing Thread " + self.name)
-        self.running = False
-        self.join()
-        logging.warning(f"{self.name} has stopped")
+# class BaseCommThread(threading.Thread):
+#     '''
+#     Parent Class to all thread manager classes.
+#     '''
+#     def __init__(self):
+#         super().__init__(name=self.__class__.__name__)
+#         self.daemon = True
+#
+#     def __enter__(self):
+#         '''
+#         Starts the thread in its own context manager block.
+#         Note: If the running thread is meant to be run indefinitely it is not
+#               recommended to use it as a context manager as once you exit the
+#               context manager, the thread will safely shut down via the
+#               __exit__() method.
+#         '''
+#         self.run()
+#
+#     def __exit__(self, exc_type, exc_value, traceback):
+#         '''
+#         Is called once the context leaves the manager, safely signals the
+#         running thread to shutdown.
+#         '''
+#         self.stop_thread()
+#
+#     # ____ Run Function ______________________________________________________
+#     def run(self):
+#         '''
+#         This function is to be overloaded in the child class. If the thread is
+#         to be run indefinitely (as in not for a fixed duration), you MUST
+#         structure this function as follows:
+#
+#         --[START]--------------------------------------------------------------
+#         self.t = threading.current_thread()  # Grab current threading context
+#         ...
+#         while getattr(self.t, 'running', True):
+#             ...
+#         ...
+#         --[END]--------------------------------------------------------------
+#
+#         This is necessary as the classes stop_thread() method can safely shut
+#         down the running thread by changing self.running to False, thus
+#         invalidating the while loop's condition.
+#         '''
+#         pass
+#
+#     # ____ Thread Killer _____________________________(Kills with kindness)___
+#     def stop_thread(self):
+#         '''
+#         This is a convenience function used to safely end the running thread.
+#         Note: This will only end the running thread if the run() function
+#               checks for the classes 'running' attribute (as demonstrated in
+#               the docstring of the run() function above).
+#               This only works if the running thread is not hanging, this will
+#               prevent the while loop from re-evaluating its condition
+#         '''
+#         logging.warning("Closing Thread " + self.name)
+#         self.running = False
+#         self.join()
+#         logging.warning(f"{self.name} has stopped")
 
 # [JEFF] The commented section below is mean to simulate the Dashboard in its
 #        own managed thread.
