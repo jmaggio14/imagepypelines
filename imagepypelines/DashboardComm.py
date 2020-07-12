acf16df9bb8ca5be7322342c9650c1c35a8b15b3
Jai Mehra <jmjaiken@gmail.com>
2020-07-11 22:27:12 -0400

    Created Class to handle connection from Pipe to Dashboard

---
+++ imagepypelines/DashboardComm.py
@@ -0,0 +1,41 @@
+"""
+# this is an example use case:
+
+ip.connect_to_dash(host, port)
+# --> would modify a class variable in the DashboardComm
+
+ip.Pipeline(tasks={})
+# --> creates instance of DashboardComm
+# self.dashcomm.update(update_params)
+    --> for host,port in self.dashes:
+        send_update(update_params)
+"""
+from .util import TCPClient
+
+
+
+def connect_to_dash(host, port):
+    """Connects every pipeline in this session to
+    """
+    DashboardComm.connect(host, port)
+
+################################################################################
+class DashboardComm(object):
+    clients = []
+    """Object to send messages from the pipelines to dashboard(s)
+    """
+
+    # --------------------------------------------------------------------------
+    @classmethod
+    def connect(cls, host, port):
+        new_client = TCPClient().connect()
+        # maybe make this a dictionary at some point?
+        cls.clients.append( new_client )
+
+    # --------------------------------------------------------------------------
+    def write(self, update_json):
+        for cli in self.clients:
+            cli.write(update_json)
+
+    # IN THE FUTURE
+    # def read()
