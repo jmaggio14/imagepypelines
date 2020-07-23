e3393df38cfb3fd90ef4a23e6a5925da22275d5b
RyanHartzell <rah3156@rit.edu>
2020-07-23 08:37:55 -0400

    Going to take a couple tries

--- docs/source/style.py
+++ docs/source/style.py
@@ -9,11 +9,15 @@ class ImagePypelinesStyle(Style):
     default_style = ""
     styles = {
             Comment: 'italic #FF5D00',
-            Keyword: 'bold #AED9E8',
+            Keyword: 'bold #BF0000',
             Name: '#FF4D00',
-            Name.Function: '#FF5D00',
+            Name.Function: '#380915',
             Name.Class: 'bold #191C3A',
-            String: '#AED9E8 bg: '
+            String: '#AED9E8',
+            Number: '#AED9E8',
+            Operator: '#BF0000',
+            Generic: '#FF5D00 bg: #EBDCC1',
+            Error: '#380915 bg: #191C3A'
             }
 
 # see Sphinx: https://www.sphinx-doc.org/en/master/usage/configuration.html#confval-pygments_style
