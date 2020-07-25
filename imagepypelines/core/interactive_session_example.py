3a64582c3ab106e8ba14544b70b6c81c7956562d
RyanHartzell <rah3156@rit.edu>
2020-07-25 09:15:52 -0400

    Darker blue on the strings and numbers. Would like lighter on dark bg

---
+++ imagepypelines/core/interactive_session_example.py
@@ -0,0 +1,10 @@
+import imagepypelines as ip
+
+# connect a bunch of dashboards
+
+# set up tasks and input data and pipelines
+
+# set up interactivity (KEEP THE NAMESPACE RUNNING AND OPEN SO WE CAN INFLUENCE PIPELINES AND RERUN THEM AND STUFF)
+# option 1
+while True:
+    pass
